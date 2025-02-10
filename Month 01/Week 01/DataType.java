5c6183e7f9c3ed5a1778d0b4ca88aa3987c77302
Aditya Prakash Pandey <adityapandey03012003@gmail.com>
2025-02-10 23:32:32 +0530

    week 01

---
+++ Month 01/Week 01/DataType.java
@@ -0,0 +1,10 @@
+// DataTypes in Java ---
+
+// Byte  - 8 Bits
+// Short - 16 Bits
+// Int   - 32 Bits
+// Long  - 64 Bits
+// char  - 16 Bits
+// Float - 32 Bits
+// Boolean - 1 Bits
+// Double - 64 Bits
